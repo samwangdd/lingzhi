8a432a592aff10e03dbf4ee4e0ba3a1d28d2e07e
王森 <wangsen@medlinker.com>
2022-02-24 16:56:19 +0800

    feat: add baseModel

---
+++ src/BaseModel/index.tsx
@@ -0,0 +1,54 @@
+import React, { forwardRef, useEffect, useImperativeHandle, useMemo, useState } from 'react';
+import { Form, Modal } from 'antd';
+import { FormInstance } from 'antd/lib/form';
+import RenderFields from '@/components/RenderFields';
+import { ItResourceITResource } from '@/api/apis';
+
+interface Props {
+  columns: any;
+  title: string;
+  onSubmit: (values) => void;
+  onSuccess?: () => void;
+  record?: ItResourceITResource;
+  editData?: any;
+}
+
+export interface BaseModelRef {
+  toggleVisible: () => void;
+  form: FormInstance<any>;
+}
+
+const BaseModel = (props: Props, ref) => {
+  const { onSuccess, record, columns, title, onSubmit } = props;
+  const [visible, setVisible] = useState(false);
+
+  const [form] = Form.useForm();
+  const toggle = () => {
+    setVisible(!visible);
+    if (!visible) form.resetFields();
+  };
+
+  useEffect(() => {
+    form.setFieldsValue(record);
+  }, [record, form]);
+
+  useImperativeHandle(ref, () => ({ toggleVisible: toggle, form: form }));
+
+  const onOk = async () => {
+    await form.validateFields();
+    const value = form.getFieldsValue();
+    const raw = { ...record, ...value };
+    onSubmit && onSubmit(raw);
+    onSuccess && onSuccess();
+  };
+
+  return (
+    <Modal forceRender visible={visible} onOk={onOk} onCancel={toggle} title={title}>
+      <Form form={form} labelCol={{ span: 6 }}>
+        <RenderFields columns={columns} wrapConfig={{ span: 20 }} />
+      </Form>
+    </Modal>
+  );
+};
+
+export default forwardRef(BaseModel);
