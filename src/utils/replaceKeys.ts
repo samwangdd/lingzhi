94bc7b53e4127099019ffadbc2229a78dae3ec21
samwangdd <wyysammore@gmail.com>
2022-03-07 15:17:09 +0800

    fix(utils): replace keys

--- src/utils/replaceKeys.ts
+++ src/utils/replaceKeys.ts
@@ -1,1 +1,13 @@
-export  function replaceKeys(s) {return s}
+export const replaceKeys: any = (obj: any | any[], keysMap: { [key: string]: string }) => {
+  if (!obj) return;
+  if (Array.isArray(obj)) {
+    return obj.map((item) => replaceKeys(item, keysMap));
+  } else {
+    return Object.keys(obj).reduce((acc, key) => {
+      const newKey = keysMap[key] || key;
+      // @ts-ignore
+      acc[newKey] = obj[key];
+      return acc;
+    }, {});
+  }
+};
